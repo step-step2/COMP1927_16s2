69f18526ec4d54c3a95c1805fe4fb24fc9ed75cd
stevenStrij <ststrijakov@gmail.com>
2016-07-27 00:22:05 +1000

    #justgroupythings

--- Groups/PairMakingMachine.py
+++ Groups/PairMakingMachine.py
@@ -1,10 +1,15 @@
 from random import shuffle
-fin = open('Tue16B')
+from sys import argv
+
+if len(argv) != 2:
+    print("You disappoint me")
+    exit()
+
+fin = open(argv[1], 'r')
+
 x = [i.strip() for i in fin]
 shuffle(x)
-
-for i in range(0, len(x) - len(x) % 2 * 3, 2):
-    print(x[i], x[i+1])
-
-if len(x) % 2:
-    print(x[-3], x[-2], x[-1])
+for i, q in zip(x[::2], x[1::2]):
+    print("%s, %s" % (i, q))
+if (len(x) % 2):
+    print(x[-1])
