284d09cf5104370850450a59acf2fb500234005c
stevenStrij <ststrijakov@gmail.com>
2016-08-15 09:59:52 +1000

    Moved files around

--- Quizzes/Week02/Balanced.c
+++ Quizzes/Week02/Balanced.c
@@ -16,74 +16,9 @@
 // valid (use the validChar method)
 // You cannot assume any particular size of the string,
 // i.e. for all you know, it is infinite or empty =)
-
-typedef struct node {
-  char ch;
-  struct node* next;
-} *Node;
-
-typedef struct stack {
-  Node top;
-} *Stack;
-
-Stack newStack() {
-  Stack s = malloc(sizeof(struct stack));
-  s->top = NULL;
-  return s;
-}
-
-Node newNode(char c) {
-  Node s = malloc(sizeof(struct node));
-  s->ch = c;
-  s->next = NULL;
-  return s;
-}
-
-void push(Stack s, char c) {
-  Node n = newNode(c);
-  n->next = s->top;
-  s->top = n;
-}
-
-char pop(Stack s) {
-  if (s->top == NULL) return '\0';
-  char c = s->top->ch;
-
-  Node d = s->top;
-  s->top = s->top->next;
-  free(d);
-
-  return c;
-}
-
 int isValid(char* str) {
-
-  Stack s = newStack();
-  for (int i = 0; str[i] != '\0'; ++i) {
-    switch (str[i]) {
-      case '[': 
-        push(s, '[');
-        break;
-      case ']':
-        if (pop(s) != '[') return INVALID;
-        break;
-      case '{':
-        push(s, '{');
-        break;
-      case '}':
-        if (pop(s) != '{') return INVALID;
-        break;
-      case '(':
-        push(s, '(');
-        break;
-      case ')':
-        if (pop(s) != '(') return INVALID;
-        break;
-    }
-  }
-
-  return VALID;
-  }
+  return INVALID;
+}
 
 int validChar(char ch) {
   switch (ch) {
