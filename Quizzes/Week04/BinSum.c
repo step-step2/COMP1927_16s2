798040ee2fa2a94bb3ca631a8871386a03689609
stevenStrij <ststrijakov@gmail.com>
2016-08-15 11:42:21 +1000

    Added string.h and instructions

--- Quizzes/Week04/BinSum.c
+++ Quizzes/Week04/BinSum.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "BinSum.h"
+
+// You are given 2 strings in the form of binary numbers, so something like
+// 1001
+// 11
+// The two strings are not necessarily the same length
+// Your job is to fill 'res' with the resultant sum of the two binary strings,
+// again, as a string
+// So after running the function on the above two binary strings, res needs
+// to contain: '1100'
+// Try not to fill all the cases immediately; start basic, work your way up
+// from there. Your first job is to be get a working version on a basic case
 
 void binSum(char* bin1, char* bin2, char* res) {
   // Remove the following set of lines when you begin working on the
