6e38e4338f20ba4d1577d0aeaba9c15acd68a191
stevenStrij <ststrijakov@gmail.com>
2016-08-29 13:30:27 +1000

    Week05 Tutorials

--- Tutorials/Week05/debug.c
+++ Tutorials/Week05/debug.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-typedef struct n { int data; struct n * next; } Node;
+typedef struct n {
+  int data; 
+  struct n * next; 
+} Node;
 typedef Node *List;
+
+void foo() {
+  while (1 == 1) {
+    printf("screw this machine\n"); 
+  }
+}
  
 int main(int argc, char *argv[])
 {
@@ -19,16 +28,17 @@ int main(int argc, char *argv[])
 		new->data = n;
 		new->next = NULL;
  
+    foo();
 		// find last node and append new node onto list
-		for (p = nums; p->next != NULL; p = p->next)
-			/* scan */ ;
+		for (p = nums; p->next != NULL; p = p->next);
 		p->next = new;
 	}
  
 	// display contents of list
 	printf("nums");
-	for (p = nums; p != NULL; p = p->next)
+	for (p = nums; p != NULL; p = p->next) {
 		printf("->%d",p->data);
+  }
 	printf("\n");
  
 	return EXIT_SUCCESS;
